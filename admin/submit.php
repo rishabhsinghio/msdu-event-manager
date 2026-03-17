417a8d305aca1db1119f9399cdbd95b3c1768ff7
Rishabh <rishabh.oppo.pad@gmail.com>
2026-03-17 13:12:34 +0530

    update 1.0.0

--- admin/submit.php
+++ admin/submit.php
@@ -1,37 +1,25 @@
 <?php
-include "db.php";
+$host="localhost";
+$user="root";
+$password="123456";
+$database="events";
 
-$thumbnail = $_FILES['image']['name'];
+$conn = mysqli_connect($host, $user, $password, $database);
+if(!$conn){
+  die("DB connection failed");
+}
+
 $title = $_POST["title"];
 $description = $_POST["description"];
 $dep = $_POST["dep"];
 $hod = $_POST["hod"];
 $venue = $_POST["venue"];
 $lastdate = $_POST["lastdate"];
-$programs = $_POST["programs"];
+//$programs = $_POST["programs"];
 $rules = $_POST["rules"];
 
-$tmpname = $_FILES['image']['tmp_name'];
-$folder = "uploads/".$thumbnail;
-
-// move file to folder
-move_uploaded_file($tmpname, $folder);
-
-$eventds = "INSERT into events (thumbnail, title, descript, depart, hod, venue, lastdate, rules) VALUES ('$thumbnail','$title', '$description', '$dep', '$hod', '$venue', '$lastdate', '$rules')";
+$eventds = "INSERT into events (title, descript, depart, hod, venue, lastdate, rules) VALUES ('$title', '$description', '$dep', '$hod', '$venue', '$lastdate', '$rules')";
 
 mysqli_query($conn,$eventds);
-
-$input = $_POST['items'];
-
-// split values by comma
-$values = explode(",", $programs);
-
-foreach ($values as $value) {
-
-    $value = trim($value); // remove spaces
-
-    $eventds = "INSERT INTO event (programs) VALUES ('$value')";
-    $conn->query($eventds);
-}
 
 ?>
